2d4f589639d844f4f0a4b663832c83520e5bb639
Amr2812 <amr.osama.elmohamady@gmail.com>
2024-11-14 04:23:32 +0200

    feat(record): support null values in fields using record's null bitmap

--- simpledb/record/Layout.java
+++ simpledb/record/Layout.java
@@ -1,86 +1,114 @@
 package simpledb.record;
 
 import java.util.*;
+
 import static java.sql.Types.*;
+
 import simpledb.file.Page;
 
 /**
  * Description of the structure of a record.
  * It contains the name, type, length and offset of
  * each field of the table.
- * @author Edward Sciore
  *
+ * @author Edward Sciore
  */
 public class Layout {
-   private Schema schema;
-   private Map<String,Integer> offsets;
-   private int slotsize;
+    private Schema schema;
+    private Map<String, Integer> offsets;
+    private Map<String, Integer> bitPositions; // for the null bitmap, positions from right to left
+    private int slotsize;
 
-   /**
-    * This constructor creates a Layout object from a schema. 
-    * This constructor is used when a table 
-    * is created. It determines the physical offset of 
-    * each field within the record.
-    * @param tblname the name of the table
-    * @param schema the schema of the table's records
-    */
-   public Layout(Schema schema) {
-      this.schema = schema;
-      offsets  = new HashMap<>();
-      int pos = Integer.BYTES; // leave space for the empty/inuse flag
-      for (String fldname : schema.fields()) {
-         offsets.put(fldname, pos);
-         pos += lengthInBytes(fldname);
-      }
-      slotsize = pos;
-   }
+    /**
+     * This constructor creates a Layout object from a schema.
+     * This constructor is used when a table
+     * is created. It determines the physical offset of
+     * each field within the record.
+     *
+     * @param schema the schema of the table's records
+     */
+    public Layout(Schema schema) {
+        this.schema = schema;
+        offsets = new HashMap<>();
+        int pos = Integer.BYTES; // leave space for the empty/inuse flag and null bitmap
+        bitPositions = new HashMap<>();
+        int bitPos = 1; // for the null bitmap, the first bit is reserved for the empty/inuse flag
+        for (String fldname : schema.fields()) {
+            offsets.put(fldname, pos);
+            pos += lengthInBytes(fldname);
+            
+            bitPositions.put(fldname, bitPos);
+            bitPos += 1;
+        }
+        slotsize = pos;
+    }
 
-   /**
-    * Create a Layout object from the specified metadata.
-    * This constructor is used when the metadata
-    * is retrieved from the catalog.
-    * @param tblname the name of the table
-    * @param schema the schema of the table's records
-    * @param offsets the already-calculated offsets of the fields within a record
-    * @param recordlen the already-calculated length of each record
-    */
-   public Layout(Schema schema, Map<String,Integer> offsets, int slotsize) {
-      this.schema    = schema;
-      this.offsets   = offsets;
-      this.slotsize = slotsize;
-   }
+    /**
+     * Create a Layout object from the specified metadata.
+     * This constructor is used when the metadata
+     * is retrieved from the catalog.
+     *
+     * @param schema   the schema of the table's records
+     * @param offsets  the already-calculated offsets of the fields within a record
+     * @param slotsize the already-calculated length of slot
+     */
+    public Layout(Schema schema, Map<String, Integer> offsets, int slotsize) {
+        this.schema = schema;
+        this.offsets = offsets;
+        this.slotsize = slotsize;
 
-   /**
-    * Return the schema of the table's records
-    * @return the table's record schema
-    */
-   public Schema schema() {
-      return schema;
-   }
+        bitPositions = new HashMap<>();
+        int bitPos = 1; // for the null bitmap, the first bit is reserved for the empty/inuse flag
+        for (String fldname : schema.fields()) {
+            bitPositions.put(fldname, bitPos);
+            bitPos += 1;
+        }
+    }
 
-   /**
-    * Return the offset of a specified field within a record
-    * @param fldname the name of the field
-    * @return the offset of that field within a record
-    */
-   public int offset(String fldname) {
-      return offsets.get(fldname);
-   }
+    /**
+     * Return the schema of the table's records
+     *
+     * @return the table's record schema
+     */
+    public Schema schema() {
+        return schema;
+    }
 
-   /**
-    * Return the size of a slot, in bytes.
-    * @return the size of a slot
-    */
-   public int slotSize() {
-      return slotsize;
-   }
+    /**
+     * Return the offset of a specified field within a record
+     *
+     * @param fldname the name of the field
+     * @return the offset of that field within a record
+     */
+    public int offset(String fldname) {
+        return offsets.get(fldname);
+    }
 
-   private int lengthInBytes(String fldname) {
-      int fldtype = schema.type(fldname);
-      if (fldtype == INTEGER)
-         return Integer.BYTES;
-      else // fldtype == VARCHAR
-         return Page.maxLength(schema.length(fldname));
-   }
+    /**
+     * Return the size of a slot, in bytes.
+     *
+     * @return the size of a slot
+     */
+    public int slotSize() {
+        return slotsize;
+    }
+
+    /**
+     * Determine the bit position of any field within a slot for the null bitmap
+     *
+     * @param fldname the name of the field
+     * @return the bit position of that field within a slot
+     */
+    public int bitPosition(String fldname) {
+        return bitPositions.get(fldname);
+    }
+
+    private int lengthInBytes(String fldname) {
+        int fldtype = schema.type(fldname);
+        if (fldtype == INTEGER)
+            return Integer.BYTES;
+        else // fldtype == VARCHAR
+            return Page.maxLength(schema.length(fldname));
+    }
 }
 
--- simpledb/record/RecordPage.java
+++ simpledb/record/RecordPage.java
@@ -1,135 +1,190 @@
 package simpledb.record;
 
 import static java.sql.Types.INTEGER;
+
 import simpledb.file.*;
 import simpledb.tx.Transaction;
 
 /**
- * Store a record at a given location in a block. 
+ * Store a record at a given location in a block.
+ *
  * @author Edward Sciore
  */
 public class RecordPage {
-   public static final int EMPTY = 0, USED = 1;
-   private Transaction tx;
-   private BlockId blk;
-   private Layout layout;
+    public static final int EMPTY = 0, USED = 1, NOTNULL = 0, NULL = 1;
+    private Transaction tx;
+    private BlockId blk;
+    private Layout layout;
 
-   public RecordPage(Transaction tx, BlockId blk, Layout layout) {
-      this.tx = tx;
-      this.blk = blk;
-      this.layout = layout;
-      tx.pin(blk);
-   }
+    public RecordPage(Transaction tx, BlockId blk, Layout layout) {
+        this.tx = tx;
+        this.blk = blk;
+        this.layout = layout;
+        tx.pin(blk);
+    }
 
-   /**
-    * Return the integer value stored for the
-    * specified field of a specified slot.
-    * @param fldname the name of the field.
-    * @return the integer stored in that field
-    */
-   public int getInt(int slot, String fldname) {
-      int fldpos = offset(slot) + layout.offset(fldname);
-      return tx.getInt(blk, fldpos);
-   }
+    /**
+     * Return the integer value stored for the
+     * specified field of a specified slot.
+     *
+     * @param fldname the name of the field.
+     * @return the integer stored in that field
+     */
+    public int getInt(int slot, String fldname) {
+        int fldpos = offset(slot) + layout.offset(fldname);
+        return tx.getInt(blk, fldpos);
+    }
 
-   /**
-    * Return the string value stored for the
-    * specified field of the specified slot.
-    * @param fldname the name of the field.
-    * @return the string stored in that field
-    */
-   public String getString(int slot, String fldname) {
-      int fldpos = offset(slot) + layout.offset(fldname);
-      return tx.getString(blk, fldpos);
-   }
+    /**
+     * Return the string value stored for the
+     * specified field of the specified slot.
+     *
+     * @param fldname the name of the field.
+     * @return the string stored in that field
+     */
+    public String getString(int slot, String fldname) {
+        int fldpos = offset(slot) + layout.offset(fldname);
+        return tx.getString(blk, fldpos);
+    }
 
-   /**
-    * Store an integer at the specified field
-    * of the specified slot.
-    * @param fldname the name of the field
-    * @param val the integer value stored in that field
-    */
-   public void setInt(int slot, String fldname, int val) {
-      int fldpos = offset(slot) + layout.offset(fldname);
-      tx.setInt(blk, fldpos, val, true);
-   }
+    /**
+     * Store an integer at the specified field
+     * of the specified slot.
+     *
+     * @param fldname the name of the field
+     * @param val     the integer value stored in that field
+     */
+    public void setInt(int slot, String fldname, int val) {
+        int bitpos = layout.bitPosition(fldname);
+        int nullmap = tx.getInt(blk, offset(slot));
+        nullmap = setBitVal(nullmap, bitpos, NOTNULL);
+        setFlag(slot, nullmap);
+        
+        int fldpos = offset(slot) + layout.offset(fldname);
+        tx.setInt(blk, fldpos, val, true);
+    }
 
-   /**
-    * Store a string at the specified field
-    * of the specified slot.
-    * @param fldname the name of the field
-    * @param val the string value stored in that field
-    */
-   public void setString(int slot, String fldname, String val) {
-      int fldpos = offset(slot) + layout.offset(fldname);
-      tx.setString(blk, fldpos, val, true);
-   }
-   
-   public void delete(int slot) {
-      setFlag(slot, EMPTY);
-   }
-   
-   /** Use the layout to format a new block of records.
-    *  These values should not be logged 
-    *  (because the old values are meaningless).
-    */ 
-   public void format() {
-      int slot = 0;
-      while (isValidSlot(slot)) {
-         tx.setInt(blk, offset(slot), EMPTY, false); 
-         Schema sch = layout.schema();
-         for (String fldname : sch.fields()) {
-            int fldpos = offset(slot) + layout.offset(fldname);
-            if (sch.type(fldname) == INTEGER)
-               tx.setInt(blk, fldpos, 0, false);
-            else
-               tx.setString(blk, fldpos, "", false);
-         }
-         slot++;
-      }
-   }
+    /**
+     * Store a string at the specified field
+     * of the specified slot.
+     *
+     * @param slot    the slot number
+     * @param fldname the name of the field
+     * @param val     the string value stored in that field
+     */
+    public void setString(int slot, String fldname, String val) {
+        int bitpos = layout.bitPosition(fldname);
+        int nullmap = tx.getInt(blk, offset(slot));
+        nullmap = setBitVal(nullmap, bitpos, NOTNULL);
+        setFlag(slot, nullmap);
 
-   public int nextAfter(int slot) {
-      return searchAfter(slot, USED);
-   }
- 
-   public int insertAfter(int slot) {
-      int newslot = searchAfter(slot, EMPTY);
-      if (newslot >= 0)
-         setFlag(newslot, USED);
-      return newslot;
-   }
-  
-   public BlockId block() {
-      return blk;
-   }
-   
-   // Private auxiliary methods
-   
-   /**
-    * Set the record's empty/inuse flag.
-    */
-   private void setFlag(int slot, int flag) {
-      tx.setInt(blk, offset(slot), flag, true); 
-   }
+        int fldpos = offset(slot) + layout.offset(fldname);
+        tx.setString(blk, fldpos, val, true);
+    }
 
-   private int searchAfter(int slot, int flag) {
-      slot++;
-      while (isValidSlot(slot)) {
-         if (tx.getInt(blk, offset(slot)) == flag)
-            return slot;
-         slot++;
-      }
-      return -1;
-   }
+    public void delete(int slot) {
+        setFlag(slot, EMPTY);
+    }
 
-   private boolean isValidSlot(int slot) {
-      return offset(slot+1) <= tx.blockSize();
-   }
+    /**
+     * Use the layout to format a new block of records.
+     * These values should not be logged
+     * (because the old values are meaningless).
+     */
+    public void format() {
+        int slot = 0;
+        while (isValidSlot(slot)) {
+            tx.setInt(blk, offset(slot), EMPTY, false);
+            Schema sch = layout.schema();
+            for (String fldname : sch.fields()) {
+                int fldpos = offset(slot) + layout.offset(fldname);
+                if (sch.type(fldname) == INTEGER)
+                    tx.setInt(blk, fldpos, 0, false);
+                else
+                    tx.setString(blk, fldpos, "", false);
+            }
+            slot++;
+        }
+    }
 
-   private int offset(int slot) {
-      return slot * layout.slotSize();
-   }
+    public int nextAfter(int slot) {
+        return searchAfter(slot, USED);
+    }
+
+    public int insertAfter(int slot) {
+        int newslot = searchAfter(slot, EMPTY);
+        if (newslot >= 0)
+            setFlag(newslot, USED);
+        return newslot;
+    }
+
+    public BlockId block() {
+        return blk;
+    }
+
+    /**
+     * set a field to null by setting the null bit in the slot's null bitmap.
+     *
+     * @param slot    the slot number
+     * @param fldname the name of the field
+     */
+    public void setNull(int slot, String fldname) {
+        int bitpos = layout.bitPosition(fldname);
+        int nullmap = tx.getInt(blk, offset(slot));
+        nullmap = setBitVal(nullmap, bitpos, NULL);
+        setFlag(slot, nullmap);
+    }
+
+    /**
+     * check ig field is null
+     *
+     * @param slot    the slot number
+     * @param fldname the name of the field
+     */
+    public boolean isNull(int slot, String fldname) {
+        int bitpos = layout.bitPosition(fldname);
+        int nullmap = tx.getInt(blk, offset(slot));
+        return getBitVal(nullmap, bitpos) == NULL;
+    }
+
+    // Private auxiliary methods
+
+    /**
+     * Set the record's empty/inuse flag and null bitmap.
+     */
+    private void setFlag(int slot, int flag) {
+        tx.setInt(blk, offset(slot), flag, true);
+    }
+
+    private int searchAfter(int slot, int flag) {
+        slot++;
+        while (isValidSlot(slot)) {
+            if (getBitVal(tx.getInt(blk, offset(slot)), 0) == flag)
+                return slot;
+            slot++;
+        }
+        return -1;
+    }
+
+    private boolean isValidSlot(int slot) {
+        return offset(slot + 1) <= tx.blockSize();
+    }
+
+    private int offset(int slot) {
+        return slot * layout.slotSize();
+    }
+
+    private int getBitVal(int val, int bitpos) {
+        return (val >> bitpos) % 2;
+    }
+
+    private int setBitVal(int val, int bitpos, int flag) {
+        int mask = (1 << bitpos);
+        if (flag == 0)
+            return val & ~mask;
+        else
+            return val | mask;
+    }
 }
 
 
